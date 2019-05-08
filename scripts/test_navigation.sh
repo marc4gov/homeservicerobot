c475bad76d7b1f46673062923b44d3c7aec65b82
Marc Minnee <marc.minnee@gmail.com>
2019-05-07 23:27:25 -0700

    map manipulation

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 DIR="$(pwd)"
 echo $DIR
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$DIR/../world/home.world" & 
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$DIR/../world/edge.world" & 
 sleep 3
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$DIR/../map/map.yaml initial_pose_a:=1.57" & 
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$DIR/../map/map2.yaml initial_pose_a:=0 initial_pose_x:=-4.0 initial_pose_y:=3.0" & 
 sleep 3
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" & 
